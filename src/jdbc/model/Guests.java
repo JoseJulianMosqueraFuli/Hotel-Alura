ccdc62be67d7934b61795d3575666dba4e1e50b7
Jose Mosquera <jojumofu@gmail.com>
2023-09-27 17:05:03 -0500

    feat: create jdbc models

--- src/jdbc/model/Guests.java
+++ src/jdbc/model/Guests.java
@@ -1,4 +1,92 @@
 package jdbc.model;
 
+import java.sql.Date;
+
 public class Guests {
+    private Integer Id;
+    private String FirstName;
+    private String LastName;
+    private Date DateOfBirth;
+    private String Nationality;
+    private String PhoneNumber;
+    private Integer ReservationId;
+
+    public Guests(String firstName, String lastName, Date dateOfBirth, String nationality, String phoneNumber,
+                  Integer reservationId) {
+        super();
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+        Nationality = nationality;
+        PhoneNumber = phoneNumber;
+        ReservationId = reservationId;
+    }
+
+    public Guests(Integer id, String firstName, String lastName, Date dateOfBirth, String nationality, String phoneNumber,
+                  Integer reservationId) {
+        super();
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+        Nationality = nationality;
+        PhoneNumber = phoneNumber;
+        ReservationId = reservationId;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(String nationality) {
+        Nationality = nationality;
+    }
+
+    public Date getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        DateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public Integer getReservationId() {
+        return ReservationId;
+    }
+
+    public void setReservationId(Integer reservationId) {
+        ReservationId = reservationId;
+    }
 }
--- src/jdbc/model/Reservation.java
+++ src/jdbc/model/Reservation.java
@@ -1,4 +1,47 @@
 package jdbc.model;
 
+import java.sql.Date;
+
 public class Reservation {
+    private Integer id;
+    private Date dateD;
+    private Date dateS;
+    private String value;
+    private String paymentMode;
+
+    public Reservation(Date dateD, Date dateS, String value, String paymentMode) {
+        this.id = null; // El id se establece como nulo por defecto
+        this.dateD = dateD;
+        this.dateS = dateS;
+        this.value = value;
+        this.paymentMode = paymentMode;
+    }
+
+    public Reservation(Integer id, Date dateD, Date dateS, String value, String paymentMode) {
+        this.id = id;
+        this.dateD = dateD;
+        this.dateS = dateS;
+        this.value = value;
+        this.paymentMode = paymentMode;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Date getDateD() {
+        return dateD;
+    }
+
+    public Date getDateS() {
+        return dateS;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getPaymentMode() {
+        return paymentMode;
+    }
 }
