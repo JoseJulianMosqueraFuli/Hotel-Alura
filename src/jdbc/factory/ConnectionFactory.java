9c29e32fbae3b75c45a1cc7be68036069438910c
Jose Mosquera <jojumofu@gmail.com>
2023-10-02 20:27:13 -0500

    feat: create views

--- src/jdbc/factory/ConnectionFactory.java
+++ src/jdbc/factory/ConnectionFactory.java
@@ -1,4 +1,32 @@
 package jdbc.factory;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+
+    public DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotelalura?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("aluraLatam1234");
+
+
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection retrieveConnection() {
+        try {
+            return this.dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al obtener la conexión a la base de datos", e);
+        }
+    }
+
 }
